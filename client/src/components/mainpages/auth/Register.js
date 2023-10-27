9463d9afad20b48f9f841b11876ef13b3a0b44cf
Eddie Ibarra <eddieibarra43@gmail.com>
2023-10-27 16:58:13 -0500

    new heroku acc

--- client/src/components/mainpages/auth/Register.js
+++ client/src/components/mainpages/auth/Register.js
@@ -114,7 +114,7 @@ function Register() {
 
                 <div className="row">
                     <button type="submit">Register</button>
-                    <Link to="/">Login</Link>
+                    <Link to="/home">Login</Link>
                 </div>
             </form>
         </div>
