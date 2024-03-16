7ae449b87ad706a6d564dc88f4105b2cdcd4c2e5
Eduardo Ibarra <eddieibarra43@gmail.com>
2024-03-16 12:14:34 -0500

    added logic for no trips planned and delete button

--- client/src/components/mainpages/home/Home.js
+++ client/src/components/mainpages/home/Home.js
@@ -1,16 +1,14 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { GlobalState } from '../../../GlobalState';
 import { Link } from 'react-router-dom'; 
+import Axios from 'axios'
 import './styles.css';
-
-// Import the updated JSON data
-import testData from './testData.json'; // Replace with the actual path to your JSON file
 
 function Home() {
   const state = useContext(GlobalState);
   const [email] = state.UserAPI.email;
-   const [local] = state.TripsAPI.trip
-
+  const [local] = state.TripsAPI.trip;
+  const [token] = state.token;
   const [trips, setTrips] = useState([]);
   const [loading, setLoading] = useState(true); // Add loading state
 
@@ -21,11 +19,25 @@ function Home() {
       setLoading(false);
     }
   }, [local, email]);
+
+  const removePost = async (id) => {
+    if (window.confirm("Do you want to delete this post?")) {
+      const res = await Axios.delete(`/api/trips/getaway/${id}`, {
+        headers: { Authorization: token }
+      });
+      alert(res.data.msg);
+    }
+  };
+
+  const doublePlay = async (id) => {
+    await removePost(id);
+    window.location.reload();
+  };
+
   // Function to render a single trip box
   const renderTrip = (vacation, index) => {
     const startDate = new Date(vacation.trip_start);
     const endDate = new Date(vacation.trip_end);
-    
     const startMonthDay = `${startDate.toLocaleString('default', { month: 'short' })} ${startDate.getDate()}`;
     const endMonthDay = `${endDate.toLocaleString('default', { month: 'short' })} ${endDate.getDate()}`;
 
@@ -38,12 +50,32 @@ function Home() {
           </div>
           <div className="trip-location">{vacation.location_address}</div>
           <button className="view-button">View</button>
+          <button onClick={() => doublePlay(vacation._id)} className="delete">Delete</button>
         </div>
       </div>
     );
   };
 
-  // Group trips by year
+  // Display loading message while data is being fetched
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  // Render "Start Planning trips" message if no trips are available
+  if (trips.length === 0) {
+    return (
+      <div className="home-container">
+        <div className="search-container">
+          <div className="search-input-container">
+            <Link to="/search" className="search-button">Create</Link>
+          </div>
+        </div>
+        <h2>Start Planning trips</h2>
+      </div>
+    );
+  }
+
+  // Render trips grouped by year
   const tripsByYear = trips.reduce((acc, vacation) => {
     const startDate = new Date(vacation.trip_start);
     const year = startDate.getFullYear();
@@ -53,11 +85,6 @@ function Home() {
     acc[year].push(vacation);
     return acc;
   }, {});
-
-  // Display loading message while data is being fetched
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="home-container">
@@ -69,7 +96,7 @@ function Home() {
 
       {Object.entries(tripsByYear).map(([year, yearTrips]) => (
         <div key={year}>
-          <div  className="year-trip">
+          <div className="year-trip">
             <h2>{year}</h2>
             <div className="open-trips-container">
               {yearTrips.map((trip, index) => renderTrip(trip, index))}
