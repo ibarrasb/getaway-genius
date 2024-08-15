235e1d367b2556663464c6f6142bf3682b729071
Eduardo Ibarra <eddieibarra43@gmail.com>
2024-08-14 22:43:42 -0500

    new ui

--- client/src/components/mainpages/tripDetails/TripForm.js
+++ client/src/components/mainpages/tripDetails/TripForm.js
@@ -3,72 +3,91 @@ import HotelSharpIcon from '@mui/icons-material/HotelSharp';
 import FlightSharpIcon from '@mui/icons-material/FlightSharp';
 import DirectionsCarFilledSharpIcon from '@mui/icons-material/DirectionsCarFilledSharp';
 import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import Divider from '@mui/material/Divider';
 
 function TripForm({ formData, editMode, handleExpenseChange, handleDateChange, handleSubmit, calculateTotalExpenses, numberOfPeople, handlePeopleChange, calculateCostPerPerson }) {
     return (
         <form className="detailed-form" onSubmit={handleSubmit}>
-        <div className={`expense-section ${editMode ? 'edit-mode' : ''}`} >
-            <label className='expense-specific'>
-                <HotelSharpIcon />
-                <a>Stay</a>
-                {editMode ? (
-                    <input
-                        type="text"
-                        name="stay_expense"
-                        value={formData.stay_expense}
-                        onChange={(e) => handleExpenseChange('stay_expense', e.target.value)}
-                        required
-                    />
-                ) : (
-                    <p>{`$${formData.stay_expense}`}</p>
-                )}
-            </label>
-            <label className='expense-specific'>
-                <FlightSharpIcon />
-                <a>Travel</a>
-                {editMode ? (
-                    <input
-                        type="text"
-                        name="travel_expense"
-                        value={formData.travel_expense}
-                        onChange={(e) => handleExpenseChange('travel_expense', e.target.value)}
-                        required
-                    />
-                ) :(
-                    <p>{`$${formData.travel_expense}`}</p>
-                )}
-            </label>
-            <label className='expense-specific'>
-                <DirectionsCarFilledSharpIcon />
-                <a>Transportation</a>
-                {editMode ? (
-                    <input
-                        type="text"
-                        name="car_expense"
-                        value={formData.car_expense}
-                        onChange={(e) => handleExpenseChange('car_expense', e.target.value)}
-                        required
-                    />
-                ) : (
-                    <p>{`$${formData.car_expense}`}</p>
-                )}
-            </label>
-            <label className='expense-specific'>
-            <LocalOfferIcon />
-            <a>Other</a>
-            {editMode ? (
-                <input
-                    type="text"
-                    name="other_expense"
-                    value={formData.other_expense}
-                    onChange={(e) => handleExpenseChange('other_expense', e.target.value)}
-                    required
-                />
-            ) : (
-                <p>{`$${formData.other_expense}`}</p>
-            )}
-        </label>
+            <div className={`expense-section ${editMode ? 'edit-mode' : ''}`} >
+
+                <div className="expense-item">
+                    <label className='expense-specific'>
+                        <HotelSharpIcon />
+                        <a>Stay</a>
+                        {editMode ? (
+                            <input
+                                type="text"
+                                name="stay_expense"
+                                value={formData.stay_expense}
+                                onChange={(e) => handleExpenseChange('stay_expense', e.target.value)}
+                                required
+                            />
+                        ) : (
+                            <p>{`$${formData.stay_expense}`}</p>
+                        )}
+                    </label>
+                    <Divider /> {/* Divider enclosed */}
+                </div>
+
+                <div className="expense-item">
+                    <label className='expense-specific'>
+                        <FlightSharpIcon />
+                        <a>Travel</a>
+                        {editMode ? (
+                            <input
+                                type="text"
+                                name="travel_expense"
+                                value={formData.travel_expense}
+                                onChange={(e) => handleExpenseChange('travel_expense', e.target.value)}
+                                required
+                            />
+                        ) : (
+                            <p>{`$${formData.travel_expense}`}</p>
+                        )}
+                    </label>
+                    <Divider /> {/* Divider enclosed */}
+                </div>
+
+                <div className="expense-item">
+                    <label className='expense-specific'>
+                        <DirectionsCarFilledSharpIcon />
+                        <a>Transportation</a>
+                        {editMode ? (
+                            <input
+                                type="text"
+                                name="car_expense"
+                                value={formData.car_expense}
+                                onChange={(e) => handleExpenseChange('car_expense', e.target.value)}
+                                required
+                            />
+                        ) : (
+                            <p>{`$${formData.car_expense}`}</p>
+                        )}
+                    </label>
+                    <Divider /> {/* Divider enclosed */}
+                </div>
+
+                <div className="expense-item">
+                    <label className='expense-specific'>
+                        <LocalOfferIcon />
+                        <a>Other</a>
+                        {editMode ? (
+                            <input
+                                type="text"
+                                name="other_expense"
+                                value={formData.other_expense}
+                                onChange={(e) => handleExpenseChange('other_expense', e.target.value)}
+                                required
+                            />
+                        ) : (
+                            <p>{`$${formData.other_expense}`}</p>
+                        )}
+                    </label>
+                    <Divider /> {/* Divider enclosed */}
+                </div>
+
             </div>
+
             {editMode && (
                 <div>
                     <label>
@@ -97,19 +116,18 @@ function TripForm({ formData, editMode, handleExpenseChange, handleDateChange, h
             {!editMode && (
                 <div>
                     <h2>Total: ${calculateTotalExpenses().toFixed(2)}</h2>
-                    {formData.trip_end && new Date(formData.trip_end) >= new Date() && ( // Check if trip_end is not in the past
-                  
-                    <label>
-                    Number of People:
-                    <input
-                        type="number"
-                        value={numberOfPeople}
-                        onChange={handlePeopleChange}
-                        min="1"
-                    />
-                    <h2>Cost per Person: ${calculateCostPerPerson()}</h2>
-                </label>                
-                )}   
+                    {formData.trip_end && new Date(formData.trip_end) >= new Date() && (
+                     <label>   {/*
+                            Number of People:
+                            <input
+                                type="number"
+                                value={numberOfPeople}
+                                onChange={handlePeopleChange}
+                                min="1"
+                            />
+                            <h2>Cost per Person: ${calculateCostPerPerson()}</h2>*/}
+                    </label> 
+                    )} 
                 </div>
             )}
             {editMode && <button className='updatetrip-button' type="submit">Update Trip</button>}
